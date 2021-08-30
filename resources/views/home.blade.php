42d8885c226cae3c7fa601cf2f920c0db5a99fd4
Anthony <anthony.mussard@gmail.com>
2021-08-30 10:13:43 +0400

    add the profiles, migration and tables / add eloquent relationship

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,9 @@
                 <div class="pr-4"><strong>10k</strong> followers</div>
                 <div class="pr-4"><strong>150</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">Michel Monsain</div>
-            <div>Wedding / event photgrapher from Reunion Island </div>
-            <div><a href="#">www.michel-photographer.com</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
