a286be062f3a1383fe4b657379cd2710a7730cee
Anthony <anthony.mussard@gmail.com>
2021-09-02 16:04:41 +0400

    default image for new profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,6 +9,11 @@ class Profile extends Model
 {
     protected $guarded = [];
     
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/7C2W3cKI2NVZCQuOcLa1Rj0mZc93skRTtJW573mz.jpg';
+        return '/storage/' . $imagePath;
+    }
     public function user ()
     {
         return $this->belongsTo(User::class);
